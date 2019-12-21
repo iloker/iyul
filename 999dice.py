951b056970a731c197f174154440139e9b4eadc5
iloker <56148431+iloker@users.noreply.github.com>
2019-12-21 13:24:01 +0700

    Update 999dice.py

--- 999dice.py
+++ 999dice.py
@@ -1,2 +1,277 @@
-import marshal,zlib,base64
-exec(zlib.decompress(base64.b64decode("eJzVGmtz2zbyu34FypsbkRb1ch6WfFVm/Irt1o+MrTSXiT0aSIIkWhSokKBlTZv/frsA+BIpWXEnvRaZWORisbtYLPYB0JnNPV8Qn30NWSAC+yHwuC2cGbODZWD7lA+9me0FNvXHc+oHrOSoAQPP9Xw6o6WR783iN6J733s+s8khHUxtciuWLlNoilyEhG8OF6prSAVDtlFn9F6KSNcc7giThgJIB0x0un7IrBI0KZZPOiQSsXbgj8MZ4+KD7DGHLBj4zlw4Hu+U2+02OXYGjBx6gvxBuhMnIOcBOaWzvuvwsQR/cGnASJdOGblecPLZC31y4wTTsqWZ1ehw2KOai1ki0MrVQdkuV6t9JkC6si2BQzaioSs6DfU6Ye68Uz7hAsSVRA8lMrkKZ30AmRp9nzSscskqzZY9TmcsmFMQt0M0a/mDzANTTr9UWjhiQrw542Z54PGRM67hIpZtUvbLFqEBmS1Hjsv2pQwENUs7ClTzGR0CmX8p4gRhJa//ANyQRM316DAwcQCymvuwWsSU61m7ur65PLio4ELXLg9OT666BxVDMej1evCHJE0C9NMdlw91QLgze1ZPNg2w5GOd1DUWPNXxDwyrk2rPkkhqqIRJrDoSqMNLT/3XgDv9p25U8gKf3pycXFWMO97Zrt3xiMrhzfnpWTdD5SAUEw/WcklIhHV8fqlQPp2dd08qxn7cc3Nye9LtHVxcVIxf6ZC6zTcbaR9NKOfMJZ8FKZpHRL6AOvmFPdApJV3cMA51NzC54+lNsYEP7JciMjcnxxXjwgP7uRVgUNNNov6xQQ7yyeGaxEalBOFcuoj+kvQ9Qaewd2D3hsKcOA8+nVjkgA/JgeuSLvNn4RM5H5JLhjtsI9Vjj4PL2UrNRbIfdo+KFrnZar9tBl8f26MP4eHgpN93muOzsN9aDj+xD4etJT3dKFS8hS6KyV9c3Uynb732pHkzabeC40+t/pKdtc/67OPZ4sqbHIfbkT/2xqyI/vEvp68/n746uPk4vRVt9ubp8ezx9vbViP729OqxeXbZnt9x4ADeAXRPQ/Aba3iVSuC14+6YQ4n2w90YnNN5KeTjsLBbuxzFtpiC4uuzYXE3WC12FoqMfQPoiaJiLWBBAOEDPW7ou9BjTISYB/v1+mKxqMH2GYICawNvVqdzp75g/RoN5k9GKaSA+3uJGNe+M3Y4OgLpiut1A0KCEYJDr9IxBBHoAb/7xfiIkAMJuUeMIw+iBRdVsZwzHE3nc9cZUIxl9acqMK+OPH9WBaEYH3hDNpR0DwYDNkeaxk59JwWpXlBQKDDELmdYPT+2neF/vnYatbbNePXjrXxuwbN82JND/1u9UWpgw+onCDU4FmYKscOFAK5/wHnAXjRK30oliGJk6vFH5gtzziCsQD5B/RA8maVC0Nj1+tQlE2c8Sb+73kK+ouID4Ztt2cgOGUEYikhhDGg2GpbEdEZEUyYdWJIzxyDgiNOgSR50dm5EkZBwzWpQC0CtwjRqhvWleW9F/XNQ15QKwHpLqgRUbPK4D8QFOEREE/+DYDvEbDbIzk40yopRcaaAqyZUJPmFlxPz4vpTDgYs83hFsOtkikrMxoooKHJm0o17jPC4dD57NHk402sFgppy3gAhP5OWFRPGaT7ATHvcczXFVqQkQI4nT+AN+k0zhb9jNAyrgprPooKPgPwHsY1GzahgZ14KmOH/SQzmBixhDCx6imjELO5iHP0KgL5UW/v3MXjk+IHQHft6eLV1X8BXYlZgZSpAStH1mQh9TkyFFK0WbjxzEdhuoFQCmkqnjTWVjkqrgDxFmdk6BPocwsHHbmJZoR+KlGU9hDOXTkLwBeMEuJiAsyOYqSdqS2NWOs0ELvxlCksuLQvQopVnPJKprXH/JTX+/otxBYJiHk1uGbjMZDx7Qoe3QrCPuUVpZSX1RNByCiYOigacwJ1rlKwsOBaEOMeU/hGSrHvtn6QCXGfmiB7dNPAGSxlyPsLUBwdXiVTInC4drscp71c4+hDLlEOct7WjfU9LmXHkfwuHYVI5YBBfiokiPf0jadGZF3IhCxCQCiFYEKi4JptBMSRA0TRgOMqOwAGAH4Ivxq0KnkeeN3WYDGuq/wNdnmNIVAxiMHq0ffRbMeQMnBaA0HfFsCPXgbh4yyDi7UeVpNmwlY+1ErTQ9yEyLlFCjNGJdB98T3hQXP4GcQWkQ4RdAzu/lTLG6DdhqoPa3AuECWHWnkDJBEM6IbVlJaWqI2XZAc9WTn6zJtiTiLqhzHMlAgelgMMWUHAeUletBqnI5ZadoJnrUCiDQKDSUEJGm8bzuHPfG0l/Io3oxYzhTQpv1bNmhgxkTSjTMKwk8F/EgUQsbPBZ0s1qOWJ2OVmsSBj0xylmK+wMyPApJPV8PA45VDkcdwE4APIrZrGFZp31E5ocTxxYP1S++T0F16BAgaAi6Pmey8AV9FwscDb0L+SOTXXPGHeQYnSSUePewrRqoIcRvprlf1+WrSyq9EH4HOlBTiXlXbTYnubWiN+1dBogoCxyEaeTASBSJxmjJU+GaUAHMykFKXLgEGIKNXxJn5QzkrHi+v17GUu2QPVGo21RrwE169ODJWTnYuiForbwHcFMw0jF4UzAVrJrd/buOecaM0771pV4UuQcN+pIO9czB8IE+jl47nrjscui+fH1mnh27NXLx6L/y0wtZR6ZIK3mlrd8J1gN8zFyitS79ZGwQMJMWMxRTuyVqAz6x7DeIbtbsI83TKYV7bF1SpQb+AdpEc9mpBqb5C9XY8T7B+gxt7tTlJ5NbdJb9cUZs0yIn02p0zIu6FSE2wWFSDpUuBxmae2raAHKXHVH3xlGoiYT4NVdjpxVZtxJZ+z59VupA3SLAjXmmuM48G4XnFPnUvlmrAj/osw8aS/P0VNz/VPZ+lpz3qYuiVaqWHB12xKl+98RLp4ZuSFIbR65GmYmNHDc3kAi43EbxNKhqS6JaiF38Ghrk0ZzzC7lCtnfNYQ+wRDL3s3Y1cCb6roe09a0mFYGD1QvUTvk1erOyCcmd75RyZGTxm6sIfr6TxBdR/PNy2lmSUY1ZhrLxmHaC1sb8rDt6tNiYtJtBi5jc20b4AJS3XFGliuGoPG0m1spY7GtKWVl1zPlrMQpLmllV66sldCC0lbCtytvFeq6EldJtK7Mle1b2onIYvEdSe2eLvXH4PuAxshBv5VZw9XS7zNd1n4id1xhY3U2oHPqEEWkhu0nvI3QAzSaUYmKQ2Sf3V0eGOYyEGxmGkJe5VSFR6FK++yFBJNOY824lIU0tyLYxfIoKlc12ReXqmmGuLfYkyPM1OHi9ucJ5NkjBfIXlPjk+44clC2tkeMdWQRFRoT3PBXjS2IM0bavqJ57ou1mN7U2mm1K4Pig4aWLZ2sm2jqTk4u8kWXNXxr/HY9M6DYc0gneOjIeTrJbwNjOItXOkzeiklaX+XI7Gf8cS19vBj8T92VmgD1DWJ6qsZ0dvHxutmIkr7W3MINNt+LJ6m+5+KlR5B+43pF7wHKWe4I0MkutD69W6o3cEZZqeC+YcjwbRV1Jd3Q0a6zeKbzI4eClGP5qU0tZWNQDklrf5T/y1euPlm2TRa8vQ3KnjdkFW1lHJ7eC+lwVzzX+Hiu74kP+Xkv7UuE2rm1Kd2o5is6ZnJWVLMqj48v44lo7Pu1ZWTAHVksX2itqyZlL4UF8kUrxWGot2Zy1rj2oLV6tF81dHQ+kla13zrvkPD7Dp+j+YHWbPHcHoVpMP7MdlQRyzLvUDcAGGTT9zULkLjrSMuR8Qq661AlT8q1XqgSI52HhNoCKM/tVWRYNWUFVCsWq/A6S5O58ozOoJISui5JQ68kLq1vhzRPsVIjzZc4O8bgwZf9dlY0XHn7iYxu/MlmL7bHB3qhFdwftvd3X/d1Bv99iw703bG+3/3qPsTZg4tc+eLATff1zMBigreG5RNx1b0NADYKF5w8L0OIuQINYP0fGxjer9BCslA26atD3mtnkNUkP8HMs9bliNpPGuhe/EZPHp7GLzuQGifKTsz82mKoPXaPZyE/zJCQWHNWd0nQJrHY7nnGp+qbRqDV0i0rU/L3kJaUjIqtgcs4dchTOKKTW6kvCj1yE0zhLOnb6dAEpN9CVH8bd8VvHpRO8ZjwL++CoHaK/+VTj3oeuS3R9fccBheyTytvd1ptXr942m3ut9m5kU+l5luSnGytnhknanz0ltFfwUhliFtEq/Q+X4Dxn")))
+import requests,json,time,sys,random,os,argparse
+import colorama
+from colorama import Fore, Back, Style
+from random import randint
+from datetime import datetime
+colorama.init(autoreset=True)
+
+
+
+parser = argparse.ArgumentParser(description='999 Dice Bot | This Is Gambling Bot Plase Take Own Your Risk')
+parser.add_argument(
+    '-c','--betset',
+    default=0,
+    help='Enter Your Betset Number (default: 0)'
+)
+my_namespace = parser.parse_args()
+
+
+
+
+
+with open('config.json', 'r') as myfile:
+      data=myfile.read()
+# parse file
+obj = json.loads(data)
+
+
+print (Style.NORMAL+Fore.MAGENTA+"      ___  _           ___       __\n     / _ \(_)______   / _ )___  / /_\n    / // / / __/ -_) / _  / _ \/ __/\n   /____/_/\__/\__/ /____/\___/\__/"+Style.NORMAL+Fore.GREEN+"\n=======================================\n"+Style.BRIGHT+Fore.GREEN+"Author By  "+Style.DIM+Fore.WHITE+": "+Style.RESET_ALL+"Kadal15\n"+Style.BRIGHT+Fore.GREEN+"Channel Yt "+Style.NORMAL+Fore.WHITE+":"+Style.RESET_ALL+" Jejaka Tutorial"+Style.BRIGHT+Fore.GREEN+"\n999 Dice Bot"+Style.NORMAL+Fore.WHITE+" | "+Style.BRIGHT+Fore.RED+"Lose Streak "+Style.NORMAL+Fore.WHITE+"|"+Style.BRIGHT+Fore.GREEN+" Win Streak\n"+Style.BRIGHT+Fore.GREEN+"support by botakberambut(hijrah) And All Termux Id Member\n"+Style.BRIGHT+Fore.GREEN+"Donate "+Style.NORMAL+Fore.WHITE+":"+Style.BRIGHT+Fore.GREEN+" BTC "+Style.RESET_ALL+"18961sqv9fPuBcEbbi1gHub8ydWePB8yaG\n"+Style.BRIGHT+Fore.GREEN+"         LTC "+Style.RESET_ALL+"LNRkk6o9h1Rh98sDW8byeH9HbeUHwNohDu\n"+Style.BRIGHT+Fore.GREEN+"         Doge "+Style.RESET_ALL+"DJG4YG3ARUkSt9e5xvHvSS3faVx3v1HM9p\n\n")
+
+hijau = Style.BRIGHT+Fore.GREEN
+
+res = Style.RESET_ALL
+abu2 = Style.NORMAL+Fore.WHITE
+ungu = Style.NORMAL+Fore.MAGENTA
+hijau2 = Style.NORMAL+Fore.GREEN
+red2 = Style.NORMAL+Fore.RED
+red = Style.BRIGHT+Fore.RED
+c = requests.session()
+
+url = "https://www.999doge.com/api/web.aspx"
+ua = {
+ "Origin": "file://",
+ "user-agent": obj["User-Agent"],
+ "Content-type": "application/x-www-form-urlencoded",
+ "Accept": "*/*",
+ "Accept-Language": "id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7",
+ "X-Requested-With": "com.reland.relandicebot"
+}
+
+def konvert(persen,taruhan):
+    global high
+    global low
+    c = str(999999 * float(persen) / 100)
+    if taruhan == "Hi" or taruhan == "hi" or taruhan == "HI":
+       n = str(c.split(".")[1])
+       pangkat = 6 - len(n)
+       low = int(int(n) * (10 ** pangkat))
+       high = 999999
+    if taruhan == "Lo" or taruhan == "LOW" or taruhan == "low" or taruhan == "Low" or taruhan == "LO":
+       low = 0
+       high = int(c.split(".")[0])
+
+
+def rev(num):
+    if (len(num) < 8):
+        panjang_nol = int(8 - len(num))
+        num = ((panjang_nol*"0")+str(num))
+        result = ("0."+num)
+    if (len(num) == 8):
+        panjang_nol = int(8 - len(num))
+        num = ((panjang_nol*"0")+str(num))
+        result = ("0."+num)
+    else:
+        len_num = len(num)
+        end = num[-8:]
+        first = num[:len_num-8]
+        result = (first+"."+end)
+    return (result)
+
+
+def dice(ws,ls):
+   if my_namespace.betset == "Auto" or my_namespace.betset == "auto" or my_namespace.betset == "AUTO":
+      urut = 0
+      jumlahulang= 0
+      while True:
+         jumlahulang+=1
+         try:
+             pesan = obj["Config"][jumlahulang]["Name Bet Set"]
+         except:
+             break
+   else:
+      urut = int(my_namespace.betset)
+
+   slp = int(obj["Config"][urut]["Interval"]) / 1000
+   limit_a = int(obj["Config"][urut]["Reset If Win"]) - 1
+   payin = int(float(obj["Config"][urut]["Base Bet"])*(10 ** 8))
+   konvert(obj["Config"][urut]["Chance"],obj["Config"][urut]["Bet"]["Bet"])
+   amount = payin
+   data = {
+      "a": "PlaceBet",
+      "s": js["SessionCookie"],
+      "PayIn": amount,
+      "Low": low,
+      "High": high,
+      "ClientSeed": randint(0,999999),
+      "Currency": "doge",
+      "ProtocolVersion": "2"
+   }
+   try:
+     r1 = c.post(url,headers=ua,data=data)
+     jsn = json.loads(r1.text)
+     jumbl = jsn["StartingBalance"] + int(jsn["PayOut"]) - int(amount)
+     jum = int(jsn["PayOut"]) - int(amount)
+     prof = (float(jsn["StartingBalance"] + int(jsn["PayOut"]) - int(amount) - jumbl)/(10 ** 8))
+     print (hijau+"\n\n\nStarting Balance",res+str((float(int(jsn["StartingBalance"]) + int(jum))/(10 ** 8))))
+     print ("Anda Menggunakan BetSet Ke "+obj["Config"][urut]["Name Bet Set"])
+     n = 0
+     burst = False
+     stats_rolebet_lose = False
+     stats_rolebet_win = False
+     menit = datetime.now().strftime('%M')
+     menit = int(menit) + int(obj["Interval"])
+     no_win = 0
+     no_lose = 0
+     total_win=0
+     total_lose=0
+     no_rolebet = 0
+     rolebet="Hi"
+     while True:
+        if obj["Config"][urut]["Max Bet"] == "OFF" or obj["Config"][urut]["Max Bet"] == "off" or obj["Config"][urut]["Max Bet"] == "Off":
+            sys.stdout.write("")
+        else:
+           if amount > int(float(obj["Config"][urut]["Max Bet"])*(10 ** 8)):
+               amount = payin
+        if obj["Config"][urut]["Bet"]["Hi / Low"]["Toggle"] == "On" or obj["Config"][urut]["Bet"]["Hi / Low"]["Toggle"] == "ON" or obj["Config"][urut]["Bet"]["Hi / Low"]["Toggle"] == "on":
+            no_rolebet +=1
+            if stats_rolebet_win is True:
+               if no_rolebet > int(obj["Config"][urut]["Bet"]["Hi / Low"]["If Win"]) - 1:
+                  rolebet = "Lo"
+               if no_rolebet > int(obj["Config"][urut]["Bet"]["Hi / Low"]["If Win"]) * 2 - 1:
+                  rolebet = "Hi"
+                  no_rolebet = 0
+            if stats_rolebet_lose is True:
+               if no_rolebet > int(obj["Config"][urut]["Bet"]["Hi / Low"]["If Lose"]) -1 :
+                  rolebet = "Lo"
+               if no_rolebet > int(obj["Config"][urut]["Bet"]["Hi / Low"]["If Lose"]) * 2 - 1:
+                  rolebet = "Hi"
+                  no_rolebet = 0
+        else:
+            rolebet = obj["Config"][urut]["Bet"]["Bet"]
+        if my_namespace.betset == "Auto" or my_namespace.betset == "AUTO" or my_namespace.betset == "auto":
+          waktu = datetime.now().strftime('%M')
+          if int(waktu) > int(menit - 1):
+             menit = int(menit) + int(obj["Interval"])
+             urut +=1
+             if urut == jumlahulang:
+                urut = 0
+             print ("Change Bet Set "+obj["Config"][urut]["Name Bet Set"]+"                           ")
+             slp = int(obj["Config"][urut]["Interval"]) / 1000
+             limit_a = int(obj["Config"][urut]["Reset If Win"]) - 1
+             payin = int(float(obj["Config"][urut]["Base Bet"])*(10 ** 8))
+
+        else:
+          urut = int(my_namespace.betset)
+
+        if obj["Config"][urut]["Random Chance"]["Toggle"] == "ON" or obj["Config"][urut]["Random Chance"]["Toggle"] == "On" or obj["Config"][urut]["Random Chance"]["Toggle"] == "on":
+           hasil_chance = round(random.uniform(float(obj["Config"][urut]["Random Chance"]["Min"]),float(obj["Config"][urut]["Random Chance"]["Max"])),2)
+           cok = len(str(hasil_chance))
+           if cok == 3:
+              sys.stdout.write("\r"+str(hasil_chance)+"   ")
+           if cok == 4:
+              sys.stdout.write("\r"+str(hasil_chance)+"  ")
+           if cok == 5:
+              sys.stdout.write("\r"+str(hasil_chance)+" ")
+           konvert(hasil_chance,str(rolebet))
+        else:
+           konvert(obj["Config"][urut]["Chance"],str(rolebet))
+        time.sleep(float(slp))
+        amount = int(amount)
+        n+=1
+        data = {
+          "a": "PlaceBet",
+          "s": js["SessionCookie"],
+          "PayIn": amount,
+          "Low": low,
+          "High": high,
+          "ClientSeed": randint(0,999999),
+          "Currency": "doge",
+          "ProtocolVersion": "2"
+        }
+        if prof > float(obj["Target Profit"]):
+           print (hijau+"\nYay.! \nProfit Mencapai Target.....!\n"+hijau+"Profit "+res+str(prof))
+           os.system("termux-toast You win "+str(prof))
+           time.sleep(1)
+           os.system("termux-toast Total Balance "+str(float(int(jsn["StartingBalance"]) + int(jum))/(10 ** 8)))
+           sys.exit()
+        r1 = c.post(url,headers=ua,data=data)
+        jsn = json.loads(r1.text)
+        prof = (float(jsn["StartingBalance"] + int(jsn["PayOut"]) - int(amount) - jumbl)/(10 ** 8))
+        jum = int(jsn["PayOut"]) - int(amount)
+        if jsn["StartingBalance"] > ws:
+           print (ungu+"["+res+str(rolebet)+ungu+"] "+hijau2+str(float(amount)/(10 ** 8)),res+str(float(int(jsn["StartingBalance"]) + int(jum))/(10 ** 8)),hijau2+"Profit",res+str(prof))
+           print (hijau+"Yay.!\nBalance Sudah Memenuhi Target.....!")
+           os.system("termux-toast Target Win Sudah Tercapai")
+           time.sleep(1)
+           os.system("termux-toast Total Balance "+str(float(int(jsn["StartingBalance"]) + int(jum))/(10 ** 8)))
+           sys.exit()
+        if jsn["StartingBalance"] < ls:
+           print (ungu+"["+res+str(rolebet)+ungu+"]"+red2+"-"+str(float(amount)/(10 ** 8)),res+str((float(int(jsn["StartingBalance"]) + int(jum))/(10 ** 8))),red2+"Lose ",res+str(prof))
+           print (Style.BRIGHT+Fore.RED+"Lose Target....!")
+           os.system("termux-toast Lose Target ")
+           time.sleep(1)
+           os.system("termux-toast Total Balance "+str(float(int(jsn["StartingBalance"]) + int(jum))/(10 ** 8)))
+           sys.exit()
+        if jsn["PayOut"] is not 0:
+           no_win +=1
+           no_lose = 0
+           bal = int(jsn["StartingBalance"]) + int(jum)
+           if prof > 0:
+             print (ungu+"["+res+str(rolebet)+ungu+"] "+hijau2+str(rev(str(amount))),res+str(rev(str(bal))),hijau2+"Profit",res+str(prof))
+           else:
+             print (ungu+"["+res+str(rolebet)+ungu+"] "+hijau2+str(rev(str(amount))),res+str(rev(str(bal))),red2+"Lose ",res+str(prof))
+
+
+        else:
+           no_win = 0
+           no_lose +=1
+           i = 0
+           burst = True
+           bal = int(jsn["StartingBalance"]) + int(jum)
+           if prof > 0:
+             print (ungu+"["+res+str(rolebet)+ungu+"]"+red2+"-"+str(rev(str(amount))),res+str(rev(str(bal))),hijau2+"Profit",res+str(prof))
+           else:
+             print (ungu+"["+res+str(rolebet)+ungu+"]"+red2+"-"+str(rev(str(amount))),res+str(rev(str(bal))),red2+"Lose ",res+str(prof))
+
+        if burst is True:
+           i+=1
+           amount = int(amount) * float(obj["Config"][urut]["If Lose"])
+           if i > limit_a:
+             i = 0
+             burst = False
+        else:
+           if n > limit_a:
+             n = 0
+             amount = payin
+           else:
+             amount = int(amount) * float(obj["Config"][urut]["If Win"])
+        if no_win > total_win:
+           stats_rolebet_win = True
+           stats_rolebet_lose = False
+           total_win +=1
+        if no_lose > total_lose:
+           stats_rolebet_lose = True
+           stats_rolebet_win = False
+           total_lose +=1
+        sys.stdout.write(hijau+"Win Streak "+res+str(total_win)+red+" Lose Streak "+res+str(total_lose)+"\r")
+
+
+
+   except:
+       print ("")
+       os.system("termux-toast Betting Stop")
+       sys.exit()
+r = c.get(url,headers=ua,data={"a": "Login","Key": "7ec7f8a2c9724b2cbb8ed75e72b47ee9","Username": obj["Account"]["Username"],"Password": obj["Account"]["Password"],"Totp": ""})
+js = json.loads(r.text)
+try:
+  print (hijau+"Balance "+abu2+": "+res+str(float(js["Doge"]["Balance"])/(10 ** 8)))
+except:
+  print ("Check Your Username And Your Password")
+  sys.exit()
+
+if float(js["Doge"]["Balance"])/(10 ** 8) > float(500000000000000000000000000.00000000):
+     print (hijau+"\n\nMaaf Versi Ini Cuma Support Untuk Balance Dibawah 500000000000000 Doge\nSilahkan Hubungi Author Untuk Full Version\nHub : +6285336117892")
+     sys.exit()
+
+
+dice(int(float(obj["Target Win"])*(10 ** 8)),int(float(obj["Lose Target"])*(10 ** 8)))
